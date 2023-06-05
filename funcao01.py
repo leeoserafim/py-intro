a491dff4ec6741333476726eee6a0c2ea0a6fc6c
leeoserafim <leeoserafim@gmail.com>
2023-06-05 19:22:29 -0300

    1

---
+++ funcao01.py
@@ -0,0 +1,9 @@
+from os import system
+system('cls')
+
+def funcao01():
+    print('funçao 01')
+
+print(__name__)
+if __name__ == 'main':
+    funcao01
---
+++ teste.py
@@ -0,0 +1,3 @@
+from random import randint
+from math import round
+
